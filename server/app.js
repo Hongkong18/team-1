7ddd9d6a5a28b1e8d945d119b211cbfefa6f9160
Abishek Puri <apuri@connect.ust.hk>
2018-10-18 13:53:37 +0800

    Fixes

--- server/app.js
+++ server/app.js
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", indexRoutes);
 app.use('/addition', additionRoutes);
 app.use('/modification', modificationRoutes);
-app.use("/getAll",getAllRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

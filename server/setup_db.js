37c0cc27bbc419f3e1347a99a129da25677ff76c
Abhinav Goyal <abhinav.goyal@live.com>
2018-10-18 11:26:16 +0800

    add setup_db

---
+++ server/setup_db.js
@@ -0,0 +1,9 @@
+// connect
+// const db = connect(''), code_for_good = null;
+// create collections
+db.createCollection("volunteer");
+db.createCollection("sponsor");
+db.createCollection("donor");
+db.createCollection("staff");
+db.createCollection("bounded_project");
+db.createCollection("ongoing_project");

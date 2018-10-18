13c4bd6083e1eabad0a08ce57ee2b7e21ed4089e
Abishek Puri <apuri@connect.ust.hk>
2018-10-18 11:21:24 +0800

    Added Addition and modification routes

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var additionRoutes = require('./routes/Addition');
+var modificationRoutes = require('./routes/Modification');
+var indexRoutes = require("./routes/index");
 var app = express();
 
 // view engine setup
@@ -20,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRoutes);
+app.use('/addition', additionRoutes);
+app.use('/modification', modificationRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

05f368bfe9bba22de3cfec420c115720537af4de
Abishek Puri <apuri@connect.ust.hk>
2018-10-18 12:35:56 +0800

    Added Bounded Project Addition Route

--- server/routes/Addition.js
+++ server/routes/Addition.js
@@ -5,16 +5,14 @@ var Bounded_Project = require("../models/Bounded_Project");
 /* GET home page. */
 router.post('/bounded_project', function(req, res, next) {
   console.log(Bounded_Project);
-  try {
+  try{
     Bounded_Project.create({
-      'name': req.body.name
-      // 'email': req.body.email,
-      // 'phone': req.body.phone,
-      // 'availability': req.body.availability,
-      // 'verified': req.body.verified
+      'name': req.body.name,
+      'brief_description': req.body.brief_description,
+      'full_description': req.body.full_description,
+      'address': req.body.address
     })
   } catch(e) {
-    console.log(typeof(Bounded_Project))
     console.log(e);
   }
 });
